4fdd277f92cc7d51ffdf21ca476c6a5d7f0bd73b
zanzero <40638417+zanzero@users.noreply.github.com>
2023-02-14 12:29:30 +0700

    Create aws_cred.py

---
+++ aws_cred.py
@@ -0,0 +1,12 @@
+class AWS_Cred:
+    access_key = "XXXXXXXXXX"
+    secret_access_key = "XXXXXXXXXXXXX"
+    region = "XXXXXXXXXXXXX" 
+
+    instance_id = "XXXXXXXXXXXXX"
+    queue_id = "XXXXXXXXXXXXX"
+    contact_flow_id = "XXXXXXXXXXXXX"
+    source_phone_number = "XXXXXXXXXXXXX"
+
+
+AWSCred = AWS_Cred()
